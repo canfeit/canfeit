7231b926bcf0d9cb074ec20b6cbe05d6bfe8aac1
灿飞 <canfeit@qq.com>
2017-11-08 09:48:24 +0800

    chore: release 0.0.5

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # canfeit
 [![Canfeit Home](https://avatars0.githubusercontent.com/u/6510795?s=40)](https://github.com/canfeit)
 
+## Install:
+`yarn add canfeit`
 ```bash
 # Install Node.js Version Manager(nvm)
 # Linux/macOS
--- main.js
+++ main.js
@@ -1,40 +1,40 @@
-const fs = require('fs-extra');
-const path = require('path');
+const fs = require('fs-extra')
+const path = require('path')
 
 const appPath = path.join('..', '..')
-const templatePath = 'template';
-const appPackage = fs.existsSync(path.join(appPath, 'package.json')) ?
-    require(path.join(appPath, 'package.json')) :
-    {};
-console.log('your current package.json', appPackage)
+const templatePath = 'template'
+const appPackage = fs.existsSync(path.join(appPath, 'package.json'))
+  ? require(path.join(appPath, 'package.json'))
+  : {}
+
 appPackage.scripts = appPackage.scripts || {}
-appPackage.scripts.release = appPackage.scripts.release || "standard-version&&yarn doc";
-appPackage.scripts.push = appPackage.scripts.push || "git push --follow-tags&&npm publish";
-appPackage.scripts.doc = appPackage.scripts.doc || "typedoc --out docs --mode file --theme minimal --readme none";
-appPackage.scripts.install = appPackage.scripts.install || "yarn global upgrade-interactive --latest&yarn upgradeInteractive --latest";
-appPackage.scripts.commitmsg = appPackage.scripts.commitmsg || "commitlint -e";
-appPackage.scripts.postinstall = appPackage.scripts.postinstall || "node -e console.log('#'.repeat(80)+'\\n\\n','docs:'+process.env.npm_package_homepage,'\\n\\n'+'#'.repeat(80))";
+appPackage.scripts.release = appPackage.scripts.release || 'standard-version&&yarn doc'
+appPackage.scripts.push = appPackage.scripts.push || 'git push --follow-tags&&npm publish'
+appPackage.scripts.doc = appPackage.scripts.doc || 'typedoc --out docs --mode file --theme minimal --readme none'
+appPackage.scripts.install = appPackage.scripts.install || 'yarn global upgrade-interactive --latest&yarn upgradeInteractive --latest'
+appPackage.scripts.commitmsg = appPackage.scripts.commitmsg || 'commitlint -e'
+appPackage.scripts.postinstall = appPackage.scripts.postinstall || "node -e console.log('#'.repeat(80)+'\\n\\n','docs:'+process.env.npm_package_homepage,'\\n\\n'+'#'.repeat(80))"
 if (!appPackage.commitlint) {
-    appPackage.commitlint = {
-        "extends": [
-            "@commitlint/config-angular"
-        ]
-    }
+  appPackage.commitlint = {
+    'extends': [
+      '@commitlint/config-angular'
+    ]
+  }
 }
 if (!appPackage.standard) {
-    appPackage.standard = {
-        "parser": "typescript-eslint-parser",
-        "plugins": [
-            "typescript"
-        ]
-    }
+  appPackage.standard = {
+    'parser': 'typescript-eslint-parser',
+    'plugins': [
+      'typescript'
+    ]
+  }
 }
 
 fs.writeFileSync(
-    path.join(appPath, 'package.json'),
-    JSON.stringify(appPackage, null, 2)
-);
+  path.join(appPath, 'package.json'),
+  JSON.stringify(appPackage, null, 2)
+)
 
 if (fs.existsSync(templatePath)) {
-    fs.copySync(templatePath, appPath);
+  fs.copySync(templatePath, appPath)
 }
